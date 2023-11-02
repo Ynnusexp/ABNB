00c0f0f3ce4b2af744c4b6bf9dcf0b20466cb80d
Light <vosunny1997@gmail.com>
2023-11-02 10:51:32 -0700

    get spot by user

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,47 +3,47 @@ const { Spot, User, SpotImage, Review, ReviewImage, Booking } = require('../../d
 const express = require('express');
 const router = express.Router();
 
-// const { requireAuth } = require('../../utils/auth.js');
-// const { where } = require('sequelize');
-// const spotimage = require('../../db/models/spotimage');
-// const spot = require('../../db/models/spot');
+const { requireAuth } = require('../../utils/auth.js');
+const { where } = require('sequelize');
 
-router.get('/', async(req, res) => {
-let spots = await Spot.findAll() //r3turn all spots
-// console.log(spots)
-let newSpots = [];
 
- for( let i = 0; i < spots.length; i++) {
-    let avgRating = await Review.findOne({
-        attribute: [
-            [sequelize.fn('AVG', sequelize.col('stars')), 'avgRating']
-        ],
-        where: {spotId: spots[i].id}
-    })
-    let previewImg = await SpotImage.findOne({
-        where: {spotId: spots[i].id, preview: true}
-    })
+// //Get all Spots
+router.get('/', async (req, res) => {
+    let spots = await Spot.findAll() //r3turn all spots
+    // console.log(spots)
+    let newSpots = [];
 
-    let newSpot = {
-        id: spots[i].id,
-        ownerId: spots[i].ownerId,
-        address: spots[i].address,
-        city: spots[i].city,
-        state: spots[i].state,
-        country: spots[i].country,
-        lat: spots[i].lat,
-        lng: spots[i].lng,
-        name: spots[i].name,
-        description: spots[i].description,
-        price: spots[i].price,
-        createdAt: spots[i].createdAt,
-        updatedAt: spots[i].updatedAt,
-        avgRating: avgRating.stars,
-        previewImg: previewImg ? previewImg.url : 'No preview image'
-    }
+    for (let i = 0; i < spots.length; i++) {
+        let avgRating = await Review.findOne({
+            attribute: [
+                [sequelize.fn('AVG', sequelize.col('stars')), 'avgRating']
+            ],
+            where: { spotId: spots[i].id }
+        })
+        let previewImage = await SpotImage.findOne({
+            where: { spotId: spots[i].id, preview: true }
+        })
 
-    // console.log(avgRating)
-    newSpots.push(newSpot)
+        let newSpot = {
+            id: spots[i].id,
+            ownerId: spots[i].ownerId,
+            address: spots[i].address,
+            city: spots[i].city,
+            state: spots[i].state,
+            country: spots[i].country,
+            lat: spots[i].lat,
+            lng: spots[i].lng,
+            name: spots[i].name,
+            description: spots[i].description,
+            price: spots[i].price,
+            createdAt: spots[i].createdAt,
+            updatedAt: spots[i].updatedAt,
+            avgRating: avgRating.stars,
+            previewImage: previewImage ? previewImage.url : 'No preview image'
+        }
+
+        // console.log(avgRating)
+        newSpots.push(newSpot)
     }
 
     return res.json({
@@ -52,12 +52,61 @@ let newSpots = [];
 })
 
 
-// router.get('/current', requireAuth, async(req, res) => {
-//     const vari = await Spot.findAll()
-//     return res.json({
-//         Spots: vari
-//     })
-//  })
+// Get all Spots owned by the Current User
+
+router.get('/current', requireAuth, async(req, res) => {
+    const user = req.user;
+    let newSpots = [];
+    const spots = await Spot.findAll({
+        where: {
+            ownerId: user.id
+        }
+    })
+
+    for (let i = 0; i < spots.length; i++) {
+        let avgRating = await Review.findOne({
+            attribute: [
+                [sequelize.fn('AVG', sequelize.col('stars')), 'avgRating']
+            ],
+            where: { spotId: spots[i].id }
+        })
+        let previewImage = await SpotImage.findOne({
+            where: { spotId: spots[i].id, preview: true }
+        })
+
+        let newSpot = {
+            id: spots[i].id,
+            ownerId: spots[i].ownerId,
+            address: spots[i].address,
+            city: spots[i].city,
+            state: spots[i].state,
+            country: spots[i].country,
+            lat: spots[i].lat,
+            lng: spots[i].lng,
+            name: spots[i].name,
+            description: spots[i].description,
+            price: spots[i].price,
+            createdAt: spots[i].createdAt,
+            updatedAt: spots[i].updatedAt,
+            avgRating: avgRating.stars,
+            previewImage: previewImage ? previewImage.url : 'No preview image'
+        }
+
+        // console.log(avgRating)
+        newSpots.push(newSpot)
+    }
+
+    return res.json({
+        Spots: newSpots
+    })
+
+ })
+
+
+
+
+
+
 
 
 module.exports = router;
