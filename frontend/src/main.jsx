1db6cb58120f02b504b6716b51df972c70d9cd6d
Light <vosunny1997@gmail.com>
2023-11-17 14:47:21 -0800

    phase 4 before OpenModalButton

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,14 @@
+// frontend/src/main.jsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 import * as sessionActions from './store/session';
+import { Modal, ModalProvider } from './context/Modal';
 
 const store = configureStore();
 
@@ -17,14 +20,48 @@ if (import.meta.env.MODE !== 'production') {
   window.sessionActions = sessionActions;
 }
 
-if (process.env.NODE_ENV !== 'production') {
-  window.store = store;
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <App />
+        <Modal />
+      </Provider>
+    </ModalProvider>
   </React.StrictMode>
 );
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { Provider } from 'react-redux';
+// import App from './App';
+// import './index.css';
+// import configureStore from './store';
+// import { restoreCSRF, csrfFetch } from './store/csrf';
+// import * as sessionActions from './store/session';
+
+// const store = configureStore();
+
+// if (import.meta.env.MODE !== 'production') {
+//   restoreCSRF();
+
+//   window.csrfFetch = csrfFetch;
+//   window.store = store;
+//   window.sessionActions = sessionActions;
+// }
+
+// if (process.env.NODE_ENV !== 'production') {
+//   window.store = store;
+// }
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <ModalProvider>
+//       <Provider store={store}>
+//         <App />
+//       </Provider>
+//     </ModalProvider>
+//   </React.StrictMode>
+// );
