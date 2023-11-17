d58d29f234f7933083879e249a2037fc8aaf9c62
Light <vosunny1997@gmail.com>
2023-11-17 13:21:54 -0800

    phase 2 complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
+
+// frontend/src/App.jsx
+
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage/LoginFormPage';
+import SignupFormPage from './components/SignupFormPage/SignupFormPage';
 import * as sessionActions from './store/session';
 
 function Layout() {
@@ -32,6 +36,10 @@ const router = createBrowserRouter([
       {
         path: '/login',
         element: <LoginFormPage />
+      },
+      {
+        path: "/signup",
+        element: <SignupFormPage />
       }
     ]
   }
@@ -63,6 +71,65 @@ export default App;
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState, useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+// import LoginFormPage from './components/LoginFormPage/LoginFormPage';
+// import * as sessionActions from './store/session';
+
+// function Layout() {
+//   const dispatch = useDispatch();
+//   const [isLoaded, setIsLoaded] = useState(false);
+
+//   useEffect(() => {
+//     dispatch(sessionActions.restoreUser()).then(() => {
+//       setIsLoaded(true)
+//     });
+//   }, [dispatch]);
+
+//   return (
+//     <>
+//       {isLoaded && <Outlet />}
+//     </>
+//   );
+// }
+
+// const router = createBrowserRouter([
+//   {
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: '/',
+//         element: <h1>Welcome!</h1>
+//       },
+//       {
+//         path: '/login',
+//         element: <LoginFormPage />
+//       }
+//     ]
+//   }
+// ]);
+
+// function App() {
+//   return <RouterProvider router={router} />;
+// }
+
+// export default App;
 
 
 
