692eb74c7994af98c0cb4bdc5dfe9692aa8c8e9c
Light <vosunny1997@gmail.com>
2023-11-19 09:45:47 -0800

    chang

--- frontend/src/components/LoginFormModal/LoginFormModal.jsx
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -12,7 +12,7 @@ function LoginFormModal() {
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
   const { closeModal } = useModal();
-  const [ isDisabled, setIsDisabled] = useState(true)
+  const [isDisabled, setIsDisabled] = useState(true);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -38,10 +38,10 @@ function LoginFormModal() {
             type="text"
             value={credential}
             onChange={(e) => {
-              setIsDisabled(e.target.value < 4 );
-              setCredential(e.target.value)
+                setIsDisabled(e.target.value.length < 4)
+                setCredential(e.target.value)
+              }
             }
-          }
             required
           />
         </label>
@@ -51,8 +51,9 @@ function LoginFormModal() {
             type="password"
             value={password}
             onChange={(e) => {
-              setIsDisabled(e.target.value < 6 );
-              setPassword(e.target.value)}
+                setIsDisabled(e.target.value.length < 6)
+                setPassword(e.target.value)
+              }
             }
             required
           />
@@ -61,8 +62,8 @@ function LoginFormModal() {
           <p>{errors.credential}</p>
         )}
         <button
-        type="submit"
-        disabled={isDisabled}
+          type="submit"
+          disabled={isDisabled}
 
         >Log In</button>
       </form>
@@ -71,62 +72,3 @@ function LoginFormModal() {
 }
 
 export default LoginFormModal;
-
-// // frontend/src/components/LoginFormPage/LoginFormPage.jsx
-
-// import { useState } from 'react';
-// import * as sessionActions from '../../store/session';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { Navigate } from 'react-router-dom';
-// import './LoginForm.css';
-
-// function LoginFormPage() {
-//   const dispatch = useDispatch();
-//   const sessionUser = useSelector((state) => state.session.user);
-//   const [credential, setCredential] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [errors, setErrors] = useState({});
-
-//   if (sessionUser) return <Navigate to="/" replace={true} />;
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     setErrors({});
-//     return dispatch(sessionActions.login({ credential, password })).catch(
-//       async (res) => {
-//         const data = await res.json();
-//         if (data?.errors) setErrors(data.errors);
-//       }
-//     );
-//   };
-
-//   return (
-//     <>
-//       <h1>Log In</h1>
-//       <form onSubmit={handleSubmit}>
-//         <label>
-//           Username or Email
-//           <input
-//             type="text"
-//             value={credential}
-//             onChange={(e) => setCredential(e.target.value)}
-//             required
-//           />
-//         </label>
-//         <label>
-//           Password
-//           <input
-//             type="password"
-//             value={password}
-//             onChange={(e) => setPassword(e.target.value)}
-//             required
-//           />
-//         </label>
-//         {errors.credential && <p>{errors.credential}</p>}
-//         <button type="submit">Log In</button>
-//       </form>
-//     </>
-//   );
-// }
-
-// export default LoginFormPage;
--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -54,4 +54,3 @@ function ProfileButton({ user }) {
 }
 
 export default ProfileButton;
-
